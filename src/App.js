f7564cbf509f1853d7175e8b6a6d1670fb60275f
Han-D-Peter <secret_wings@naver.com>
2023-11-07 16:56:30 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "react-openlayers7/dist/index.css";
+import { icon } from "react-openlayers7";
+import CSVReader from "./component/CSVReader";
+
+icon.marker = "/images/marker-basic.png";
+icon.selected = "/images/marker-selected.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <CSVReader />
+    </main>
   );
 }
 
